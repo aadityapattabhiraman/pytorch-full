7acecad602911657279d2cac9e3e5ed8cd2971ea
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-03-15 21:11:00 +0530

    tensors.py

--- tensors.py
+++ tensors.py
@@ -43,3 +43,21 @@ print(tensor)
 
 t1 = torch.cat([tensor, tensor, tensor], dim=1)
 print(t1)
+
+y1 = tensor @ tensor.T
+y2 = tensor.matmul(tensor.T)
+y3 = torch.rand_like(y1)
+torch.matmul(tensor, tensor.T, out=y3)
+
+z1 = tensor * tensor
+z2 = tensor.mul(tensor)
+z3 = torch.rand_like(tensor)
+torch.mul(tensor, tensor, out=z3)
+
+agg = tensor.sum()
+agg_item = agg.item()
+print(agg_item, type(agg_item))
+
+print(f"{tensor}\n")
+tensor.add_(5)
+print(tensor)
