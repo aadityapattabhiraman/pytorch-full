ce6e8fef406b339c223f791c85e57583c036d4cf
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-03-15 20:41:10 +0530

    tensors.py

---
+++ tensors.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import torch
+import numpy as np
+
+
+data = [[1, 2], [3, 4]]
+x_data = torch.tensor(data)
+print(x_data)
+
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
+print(x_np)
