6a23268250d8e08c32e7e3e4b2d10b8ce1d6531a
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-03-16 22:43:51 +0530

    aytograd.py

--- autograd.py
+++ autograd.py
@@ -16,3 +16,14 @@ print(f"Gradient function for loss: {loss.grad_fn}")
 loss.backward()
 print(w.grad)
 print(b.grad)
+
+z = torch.matmul(x, w) + b
+print(z.requires_grad)
+
+with torch.no_grad():
+    z = torch.matmul(x, w) + b
+print(z.requires_grad)
+
+z = torch.matmul(x, w) + b
+z_det = z.detach()
+print(z_det.requires_grad)
