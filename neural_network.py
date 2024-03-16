5da2cd3a387c90b2cc80bc4d72d9fc02c238aa42
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-03-16 09:10:17 +0530

    neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -38,3 +38,33 @@ logits = model(x)
 pred_prob = nn.Softmax(dim=1)(logits)
 y_pred = pred_prob.argmax(1)
 print(f"Predicted class: {y_pred}")
+
+input_image = torch.rand(3, 28, 28)
+flatten = nn.Flatten()
+flat_image = flatten(input_image)
+print(flat_image.size())
+
+layer1 = nn.Linear(in_features=28*28, out_features=20)
+hidden1 = layer1(flat_image)
+print(hidden1.size())
+
+print(f"Before ReLU: {hidden1}\n\n")
+hidden1 = nn.ReLU()(hidden1)
+print(f"After ReLU: {hidden1}")
+
+seq_modules = nn.Sequential(
+    flatten,
+    layer1,
+    nn.ReLU(),
+    nn.Linear(20, 10)
+)
+input_image = torch.rand(3,28,28)
+logits = seq_modules(input_image)
+
+softmax = nn.Softmax(dim=1)
+pred_probab = softmax(logits)
+
+print(f"Model structure: {model}\n\n")
+
+for name, param in model.named_parameters():
+    print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
