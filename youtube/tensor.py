0306866a569cb7fb583fdf02062622d6713ab563
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-03-17 16:38:26 +0530

    tensor.py

--- youtube/tensor.py
+++ youtube/tensor.py
@@ -16,3 +16,34 @@ print(ones)
 torch.manual_seed(47)
 random = torch.rand(2, 3)
 print(random)
+
+torch.manual_seed(47)
+random1 = torch.rand(2, 3)
+print(random1)
+
+random2 = torch.rand(2, 3)
+print(random2)
+
+torch.manual_seed(47)
+random3 = torch.rand(2, 3)
+print(random3)
+
+x = torch.empty(2, 2, 3)
+print(x.shape)
+print(x)
+
+empty_like_x = torch.empty_like(x)
+print(empty_like_x.shape)
+print(empty_like_x)
+
+zeros_like_x = torch.zeros_like(x)
+print(zeros_like_x.shape)
+print(zeros_like_x)
+
+ones_like_x = torch.ones_like(x)
+print(ones_like_x.shape)
+print(ones_like_x)
+
+rand_like_x = torch.rand_like(x)
+print(rand_like_x.shape)
+print(rand_like_x)
