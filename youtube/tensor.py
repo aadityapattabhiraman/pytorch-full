21703537bb7c08c8565c7320559bf01a0be4057e
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-03-17 18:28:39 +0530

    tensors.py

--- youtube/tensor.py
+++ youtube/tensor.py
@@ -65,3 +65,25 @@ print(b)
 
 c = b.to(torch.int32)
 print(c)
+
+# bool ,int8, int16, int32, int64, half, uint8, float, double, bfloat
+ones = torch.zeros(2, 2) + 1
+twos = torch.ones(2, 2) * 2
+threes = (torch.ones(2, 2) * 7 - 1) / 2
+fours = twos ** 2
+sqrt2s = twos ** 0.5
+
+print(ones)
+print(twos)
+print(threes)
+print(fours)
+print(sqrt2s)
+
+powers2 = twos ** torch.tensor([[1, 2], [3, 4]])
+print(powers2)
+
+fives = ones + fours
+print(fives)
+
+dozens = threes * fours
+print(dozens)
