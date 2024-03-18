68025b98f4b269ae64fe33f631ee6f77e85b05b4
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-03-18 22:04:12 +0530

    finetune.py

--- image/finetune.py
+++ image/finetune.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python3
 
+import os
+import torch
+from torchvision.io import read_image
+from torchvision.ops.boxes import masks_to_boxes
+from torchvision import tv_tensors
+from torchvision.transforms.v2 import functional as F
+
+
+class dataset(torch.utils.data.Dataset):
+
+    def __init__(self, root, transforms):
+
+        self.root = root
+        self.transforms = transforms
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
+
+    def __getitem__(self, idx):
+
+        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, "PedMasks",self. masks[idx])
+        img = read_image(img_path)
+        mask = read_image(mask_path)
+
+        obj_ids = torch.unique(mask)
+        obj_ids = obj_ids[1:]
+        num_objs = len(obj_ids)
+
+        masks = (mask == obj_ids[:, None, None]).to(dtype=torch.uint8)
+        boxes = masks_to_boxes(masks)
